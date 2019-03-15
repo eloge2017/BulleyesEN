ad65af266b2c3e01e7e63791401a6c776298b10d
eloge ndja <eloge.ndja@uamaker.nyc>
2019-03-15 09:38:15 -0400

    added custom function

--- bullsEyesEN.py
+++ bullsEyesEN.py
@@ -3,9 +3,12 @@ from graphics import*
 bEyesWin = GraphWin ("bEyes", 500, 500)
 bEyesWin.setCoords(0, 0, 500, 500)
 
-rCir = Circle (Point(250,250), 100)
-rCir.setFill("red")	
-rCir.draw(bEyesWin)
+def draw_Cir(cX, cY, size, color, Win):
+    circle = Circle(Point(cX, cY),70)
+    circle.setFill(color)
+    circle.draw(Win)
+
+draw_Cir(250, 250,70, "red", bEyesWin)
 
 bEyesWin.getMouse ()
 bEyesWin.close ()
