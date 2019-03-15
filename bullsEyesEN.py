2c3585a40b5204c09a72f0790a9d24a1b41eed83
eloge ndja <eloge.ndja@uamaker.nyc>
2019-03-15 10:12:49 -0400

    the bulleye created without color

--- bullsEyesEN.py
+++ bullsEyesEN.py
@@ -3,12 +3,14 @@ from graphics import*
 bEyesWin = GraphWin ("bEyes", 500, 500)
 bEyesWin.setCoords(0, 0, 500, 500)
 
-def draw_Cir(cX, cY, size, color, Win):
-    circle = Circle(Point(cX, cY),70)
-    circle.setFill(color)
+def draw_Cir(cX, cY, size, color, Win,x):
+    circle = Circle(Point(cX, cY),(size/6) * (x * 6))
+    #circle.setFill(color)
     circle.draw(Win)
-
-draw_Cir(250, 250,70, "red", bEyesWin)
+for i in range (6): 
+    size=10
+    draw_Cir(250, 250, size, "red", bEyesWin, i)
+    
 
 bEyesWin.getMouse ()
 bEyesWin.close ()
