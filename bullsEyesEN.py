12f30913b7e00aa0cf6cd74b0614821ea18963d0
eloge ndja <eloge.ndja@uamaker.nyc>
2019-03-15 10:51:24 -0400

    the bulleye appears many times in random locations

--- bullsEyesEN.py
+++ bullsEyesEN.py
@@ -1,5 +1,5 @@
 from graphics import*
-
+import random
 bEyesWin = GraphWin ("bEyes", 500, 500)
 bEyesWin.setCoords(0, 0, 500, 500)
 
@@ -12,9 +12,9 @@ def draw_Cir(cX, cY, size, color, Win):
             circle.setFill(color_rgb(0,0,0))
         circle.draw(Win)
    
-size=200
-draw_Cir(250, 250, size, "red", bEyesWin)
-    
+size=70
+for z in range(6):
+    draw_Cir(random.randint(0,500),random.randint(0,500), size, "red", bEyesWin)
 
 bEyesWin.getMouse ()
 bEyesWin.close ()
