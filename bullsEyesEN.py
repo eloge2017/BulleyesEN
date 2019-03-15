903ca38c849edf2e2c7e47bdc01cfc70bfb13abb
eloge ndja <eloge.ndja@uamaker.nyc>
2019-03-15 10:34:33 -0400

    bulleye has a custom function

--- bullsEyesEN.py
+++ bullsEyesEN.py
@@ -3,13 +3,17 @@ from graphics import*
 bEyesWin = GraphWin ("bEyes", 500, 500)
 bEyesWin.setCoords(0, 0, 500, 500)
 
-def draw_Cir(cX, cY, size, color, Win,x):
-    circle = Circle(Point(cX, cY),(size/6) * (x * 6))
-    #circle.setFill(color)
-    circle.draw(Win)
-for i in range (6): 
-    size=10
-    draw_Cir(250, 250, size, "red", bEyesWin, i)
+def draw_Cir(cX, cY, size, color, Win):
+     for i in range (6): 
+        circle = Circle(Point(cX, cY),(size/6) * (i - 6))
+        if i%2 == 0:
+            circle.setFill(color)
+        else:
+            circle.setFill(color_rgb(0,0,0))
+        circle.draw(Win)
+   
+size=200
+draw_Cir(250, 250, size, "red", bEyesWin)
     
 
 bEyesWin.getMouse ()
